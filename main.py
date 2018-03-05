e15318227e9e58c1c020ec230f7d56346c7572ff
Álvaro Beleño <alvarobrod@outlook.es>
2018-03-05 09:48:35 +0100

    Modificado fichero main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ os.system('clear')
 
 menu = '1.- Muestra los efectos empleados.\n2.- ¿Cuántas pistas de Audio y MIDI hay en total?\
 		\n3.- Muestra las pistas que contienen una cadena.\n4.- Muestra las pistas que tienen algún efecto que contenga una cadena.\
-		\n5.- Contar cuántas pistas (Audio o MIDI) tienen un efecto que contenga una cadena.'
+		\n5.- Contar cuántas pistas (Audio o MIDI) tienen un efecto que contenga una cadena.\
+		\n0.- Salir'
 
 opcion = input('Selecciona una opción: ')
+
+while 
